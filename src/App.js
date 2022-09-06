1c2cf27fa2c3bdcca14a9c812824591e7702f97d
Gustavo Ramirez <gramirez@overall.com.pe>
2022-09-06 11:43:12 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, {useState} from 'react'
 import FunctionContextComponent from './FunctionContextComponent'
 import ClassContextComponent from './FunctionContextComponent'
-
-export const ThemeContext = React.createContext()
+import {ThemeProvider} from './ThemeContext'
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(true)
-
-  function toggleTheme(){
-    setDarkTheme(prevDarkTheme => !prevDarkTheme)
-  }
-
   return (
     <>
-      <ThemeContext.Provider value={darkTheme}>
-        <button onClick={toggleTheme}>Toggle Theme</button>
+      <ThemeProvider>
         <FunctionContextComponent/>
-        <ClassContextComponent/>
-      </ThemeContext.Provider>
+      </ThemeProvider>
     </>
   )
 }
--- src/FunctionContextComponent.js
+++ src/FunctionContextComponent.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useContext } from 'react';
-import { ThemeContext } from './App'
+import React from 'react'
+import { useTheme, useThemeUpdate } from './ThemeContext'
 
 const FunctionContextComponent = (props) => {
-    const darkTheme   = useContext(ThemeContext)
+    const darkTheme = useTheme()
+    const toggleTheme = useThemeUpdate()
     const themeStyles = {
         backgroundColor: darkTheme ? '#333' : '#CCC',
         color: darkTheme ? '#CCC' : '#333',
@@ -11,8 +11,11 @@ const FunctionContextComponent = (props) => {
         margin: '2rem'
     }
 
-    return(
-        <div style={themeStyles}>Function Theme</div>
+    return (
+        <>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+            <div style={themeStyles}>Function Theme</div>
+        </>
     );
 }
 
